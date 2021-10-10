b0a6ad2a393b9f86788c996b3e2364add2e2674a
sreeprasanth <86183385+sreeprasanth@users.noreply.github.com>
2021-10-10 08:15:34 -0700

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [{path:'', component:HomeComponent},
 {path:'home', component:HomeComponent},
-{path:'signup', component:SignupComponent},
+{path:'signup',component:SignupComponent},
 {path:'login', component:LoginComponent}];
 
 @NgModule({
