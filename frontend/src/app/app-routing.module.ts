5dabd90213100358faa58af27dfdc561d4b072cd
Annette27 <86048560+Annette27@users.noreply.github.com>
2021-10-16 19:37:38 +0530

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,11 +4,19 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { AuthGuard } from './auth.guard';
+import { ApproveComponent } from './approve/approve.component';
+import { TrainerlistComponent } from './trainerlist/trainerlist.component';
+import { AddcoursesComponent } from './addcourses/addcourses.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 const routes: Routes = [{path:'', component:HomeComponent},
 {path:'home', component:HomeComponent},
+{path:'aboutus', component:AboutusComponent},
 {path:'signup',component:SignupComponent},
-{path:'login', component:LoginComponent}];
+{path:'login', component:LoginComponent},
+{path:'approve', component:ApproveComponent,canActivate:[AuthGuard]},
+{path:'trainers', component:TrainerlistComponent,canActivate:[AuthGuard]},
+{path:'courses', component:AddcoursesComponent,canActivate:[AuthGuard]}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
