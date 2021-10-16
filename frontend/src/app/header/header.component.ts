1a04091674442e76f67f5b4d5e0a969b725590fd
Annette27 <86048560+Annette27@users.noreply.github.com>
2021-10-16 19:40:01 +0530

    Update header.component.ts

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router:Router, public authservice:AuthService) { }
+  logOut(){
+    localStorage.removeItem('token');
+    localStorage.removeItem('token1');
+    localStorage.removeItem('approveId');
+    localStorage.removeItem('allocationId');
 
+
+
+    this.router.navigate(['/'])
+
+  }
   ngOnInit(): void {
   }
 
