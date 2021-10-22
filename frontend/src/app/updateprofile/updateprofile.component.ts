e63233ced7bf10317ae3873aa22e1c3e5f26b419
Annette27 <86048560+Annette27@users.noreply.github.com>
2021-10-22 19:45:53 +0530

    Update updateprofile.component.ts

--- frontend/src/app/updateprofile/updateprofile.component.ts
+++ frontend/src/app/updateprofile/updateprofile.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../profile.service';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { ApplistService } from '../applist.service';
+import { CourseModel } from '../models/courses';
+import { Subscription } from 'rxjs';
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-updateprofile',
   templateUrl: './updateprofile.component.html',
@@ -22,17 +27,27 @@ export class UpdateprofileComponent implements OnInit {
     imagepath:'',
     id:''
    }
-  constructor(private profilservice:ProfileService,private router:Router) { }
-
+  constructor(private profilservice:ProfileService,private router:Router,public authservice:AuthService,private AppService :ApplistService) { }
+  courses:CourseModel[]=[];
+  private courseSubscription : Subscription;
   ngOnInit(): void {
     let ProfileId = localStorage.getItem("editProfileId");
     this.profilservice.logiUser(ProfileId).subscribe((data)=>{
       this.Profile=JSON.parse(JSON.stringify(data));
-
+console.log(this.Profile)
   })
-
+  this.AppService.getCourses();
+  this.courseSubscription=this.AppService
+  .getCoursesStream()
+  .subscribe((courses:CourseModel[])=>{
+  this.courses=courses;
+  })
 }
-
+public noWhitespaceValidator(control: FormControl) {
+  const isWhitespace = (control.value || '').trim().length === 0;
+  const isValid = !isWhitespace;
+  return isValid ? null : { 'whitespace': true };
+}
 images:any
 filename:string
 url: any = undefined;
@@ -53,6 +68,10 @@ editProfile()
 
   this.profilservice.editProfile(this.Profile);   
   alert("Success");
+  this.router.navigate(['profile']);
+}
+cancel(Profile:any){
+  alert("Cancelled");
   this.router.navigate(['profile']);
 }
 }
