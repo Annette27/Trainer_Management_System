6c0a564830ea568ab023bf1f2dc4c306b09c8221
Annette27 <86048560+Annette27@users.noreply.github.com>
2021-10-23 21:39:39 +0530

    Update viewallocation.component.ts

--- frontend/src/app/viewallocation/viewallocation.component.ts
+++ frontend/src/app/viewallocation/viewallocation.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { AllocationModel } from '../models/allocation';
 import { ProfileService } from '../profile.service';
 
 @Component({
@@ -10,28 +11,18 @@ import { ProfileService } from '../profile.service';
 })
 export class ViewallocationComponent implements OnInit {
 
-  allocate = {
-    name: '',
-    id:'',
-    type:'',
-    course:'',
-    startdate:'',
-    enddate:'',
-    time:'',
-    courseid:'',
-    batch:'',
-    venue:'',
-        }
+   allocations:AllocationModel[]=[];
   constructor(private profservice:ProfileService,private router:Router, public authservice:AuthService) { }
 
   ngOnInit(): void {
        this.profservice.UserAlloc(localStorage.getItem('foralloc')).subscribe(res=>{
       if((JSON.parse(JSON.stringify(res)).error)=="No Allocations to view"){
         alert("No Allocations to view")
-        this.router.navigate(['empty'])
+        // this.router.navigate(['empty'])
       }
       else{
-        this.allocate=(JSON.parse(JSON.stringify(res))).data
+        this.allocations=(JSON.parse(JSON.stringify(res))).data
+        console.log(this.allocations)
       }
       
     })
