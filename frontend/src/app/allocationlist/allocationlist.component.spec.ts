2f58a6d1b2d74662701200519f6157948e14c363
Annette27 <86048560+Annette27@users.noreply.github.com>
2021-10-23 17:51:36 +0530

    Create allocationlist.component.spec.ts

---
+++ frontend/src/app/allocationlist/allocationlist.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AllocationlistComponent } from './allocationlist.component';
+
+describe('AllocationlistComponent', () => {
+  let component: AllocationlistComponent;
+  let fixture: ComponentFixture<AllocationlistComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AllocationlistComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AllocationlistComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
