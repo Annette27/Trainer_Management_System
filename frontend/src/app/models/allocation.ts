4c53bfb2552a241a7f2b3f217cb115f301f02987
Annette27 <86048560+Annette27@users.noreply.github.com>
2021-10-23 17:51:58 +0530

    Create allocation.ts

---
+++ frontend/src/app/models/allocation.ts
@@ -0,0 +1,13 @@
+export interface AllocationModel{
+
+    name: string,
+   course:string,
+   id:string,
+   courseid:string,
+   batch:string,
+   startdate:string,
+   enddate:string,
+   venue:string,
+   time:string
+
+}
